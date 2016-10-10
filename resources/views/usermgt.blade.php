180c1996d3874ca8f282bd9d6fc1458f6e31d29b
RealRalphBausas <iamralph18@gmail.com>
2016-10-10 10:57:43 +0800

    I'm the man who broke the build

--- resources/views/usermgt.blade.php
+++ resources/views/usermgt.blade.php
@@ -1,8 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
+<link href="css/style2.css" rel="stylesheet">
 
-<div class="container">
+<!-- Data Tables -->
+    <link href="css/plugins/dataTables/dataTables.bootstrap.css" rel="stylesheet">
+    <link href="css/plugins/dataTables/dataTables.responsive.css" rel="stylesheet">
+    <link href="css/plugins/dataTables/dataTables.tableTools.min.css" rel="stylesheet">
+<!-- Mainly scripts -->
+    <script src="js/jquery-2.1.1.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="js/inspinia.js"></script>
+    <!-- Data Tables -->
+    <script src="js/plugins/dataTables/jquery.dataTables.js"></script>
+    <script src="js/plugins/dataTables/dataTables.bootstrap.js"></script>
+    <script src="js/plugins/dataTables/dataTables.responsive.js"></script>
+    <script src="js/plugins/dataTables/dataTables.tableTools.min.js"></script>
+
     <div class="row">
     <div class="flash-message">
               @foreach(['danger','warning','success','info'] as $message)
@@ -19,36 +37,41 @@
                 <div class="panel-body">
                 MAH NIGGA
 
-                <!-- Modal -->
-              <div id="modalEdit" class="modal fade" role="dialog" aria-hidden="true">
-                <div class="modal-dialog">
-
-                  <!-- Modal content-->
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <button type="button" class="close" data-dismiss="modal">&times;</button>
-                      <h4 class="modal-title">Modal Header</h4>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalEdit">
+                                FlipInY effect
+                            </button>
+                            <div class="modal inmodal" id="modalEdit" tabindex="-1" role="dialog" aria-hidden="true">
+                              <br><br><br><br><br>
+                                <div class="modal-dialog">
+                                    <div class="modal-content animated flipInY">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                                            <h4 class="modal-title">Modal title</h4>
+                                            <small class="font-bold">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</small>
+                                        </div>
+                                        <div class="modal-body">
+                                            <label for="userID">User ID:</label>
+                                            <input type="text" class="form-control" id="userID" name="studentID">
+                                            <label for="fName">First Name:</label>
+                                            <input type="text" class="form-control" id="fName" name="studentID">
+                                            <label for="lName">Last Name:</label>
+                                            <input type="text" class="form-control" id="lName" name="studentID">
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-white" data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary">Save changes</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                  <div class="ibox float-e-margins">
+                    <div class="ibox-title">
+                        <h5>Basic Data Tables example with responsive plugin</h5>
                     </div>
-                    <div class="modal-body">
-                      <div class="form-group">
-                      <label for="userID">User ID:</label>
-                      <input type="text" class="form-control" id="userID" name="studentID">
-                      <label for="fName">First Name:</label>
-                      <input type="text" class="form-control" id="fName" name="studentID">
-                      <label for="lName">Last Name:</label>
-                      <input type="text" class="form-control" id="lName" name="studentID">
-                      </div>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    </div>
-                  </div>
+                    <div class="ibox-content">
 
-                </div>
-              </div>
-
-                <table cellspacing="0" class="table table-striped" id="usertable" width="100%">
-                  <thead>
+                    <table class="table table-striped table-bordered table-hover dataTables-example" id="usertable">
+                    <thead>
                     <tr>
                       <th>#</th>
                       <th>School ID</th>
@@ -58,7 +81,7 @@
                       <th>Created At</th>
                     </tr>
                   </thead>
-                  <tbody>
+                    <tbody>
                     @foreach($users as $user)
                       <tr>
                         <th>{{$tableCounter++}}</th>
@@ -77,22 +100,35 @@
                       </tr>
                     @endforeach
                   </tbody>
-                </table>
+                    <tfoot>
+                    <tr>
+                      <th>#</th>
+                      <th>School ID</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Actions</th>
+                      <th>Created At</th>
+                    </tr>
+                  </tfoot>
+                    </table>
+
+                    </div>
+                </div>
+
 
                 </div>
             </div>
         </div>
     </div>
-</div>
+
 
 
 <script type="text/javascript">
   $(document).ready(function() {
-    $('#usertable').DataTable({
+    $('#usertable').dataTable({
       responsive: true,
       "scrollX":true
     });
-
 
     $('.btn-edit').on('click', function(){
 
@@ -107,6 +143,11 @@
         
         
     });
+ $('.dataTables-example').dataTable({
+                responsive: true
+            });   
+
+
 } );
 </script>
 @endsection
