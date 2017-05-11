0ff3a1a2ab977787442b997ff5b6e8f3380f6160
andregabo <andregabo18@yahoo.com>
2017-05-11 16:58:49 +0800

    Login controller reverted

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
-use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     /*
@@ -42,11 +41,4 @@ class LoginController extends Controller
         return 'studentID';
     }
     
-    public function authenticate()
-    {
-        if (Auth::attempt(['email' => $email, 'password' => $password, 'isDeleted'=> null])) {
-            // Authentication passed...
-            return redirect()->intended('home');
-        }
-    }
 }
